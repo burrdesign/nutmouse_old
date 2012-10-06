30a078ff11b1350dd01dd77b6f5d66f0cb47b02a
julian <julian_burr@gmx.de>
2012-10-06 19:17:52 +0200

    Auf SQLManager umgestellt

--- core/system/modules/admin/auth/checklogin.php
+++ core/system/modules/admin/auth/checklogin.php
@@ -8,10 +8,15 @@
 	
 	if($_POST['adminlogin_submit']){
 		//Loginformular wurde abgesendet => verarbeiten + ggf. Anmeldung initialisieren
-		$admin_user = sqlresult("
+		$sql = new SQLManager();
+		$sql->setQuery("
 			SELECT * FROM bd_admin_user
-			WHERE adminUserName = '".$_POST['adminlogin_user']."' AND adminUserPassword = '".md5($_POST['adminlogin_password'])."'
-			LIMIT 1");
+			WHERE adminUserName = '{{user}}' AND adminUserPassword = '{{password}}'
+			LIMIT 1
+			");
+		$sql->bindParam("{{user}}",$_POST['adminlogin_user']);
+		$sql->bindParam("{{password}}",md5($_POST['adminlogin_password']));
+		$admin_user = $sql->result();
 		
 		if($admin_user['adminUserKey']){
 			$_SESSION['BURRDESIGN']['ADMIN']['loggedin'] = $admin_user['adminUserKey'];
