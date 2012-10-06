0dd69c0975fbe6ed0846375e30844e9056fedc84
julian <julian_burr@gmx.de>
2012-10-06 19:17:37 +0200

    Unnötige Funktionen entfernt

--- core/system/functions.php
+++ core/system/functions.php
@@ -7,31 +7,7 @@
 	***********************************************/
 	
 	include_once($_SERVER['DOCUMENT_ROOT']."/core/classes/SQLManager.php");
-	
-	function sqlquery($befehl,$befehl_typ="select"){
-		//Allgemeine SQL-Abfrage
-		$mysqlhost = "localhost";
-		$mysqluser = "d0148efd";
-		$mysqlpwd = "TwCWJB7ce2ErGUG9";
-		$mysqldb = "d0148efd";
-	
-		$connection = mysql_connect($mysqlhost,$mysqluser,$mysqlpwd) OR die ('<pre>Verbindungsversuch fehlgeschlagen</pre>');
-		mysql_select_db($mysqldb,$connection) OR die ('<pre>Konnte die Datenbank nicht w&auml;hlen</pre>');
-		
-		$result = mysql_query($befehl) OR die ("<pre>Anfrage nicht erfolgreich\n\n$befehl\n\n".mysql_error()."</pre>");
-		
-		return $result;
-	}
-	
-	
-	function sqlresult($befehl){
-		//SQL-Abfrage mit einem Ergebnis => Ergebnis direkt in Array laden und zutück geben
-		$abfrage = sqlquery($befehl,'select');
-		while ($row = mysql_fetch_array($abfrage)) {
-			$ergebnis = $row;
-		}
-		return $ergebnis;
-	}
+	include_once($_SERVER['DOCUMENT_ROOT']."/core/classes/Log.php");
 	
 	function get_include($module_path){
 		$output = "";
@@ -41,12 +17,6 @@
 			$output = ob_get_contents();
 		ob_end_clean();
 		return $output;
-	}
-	
-	function loadconfig(){
-		$config['HOST'] = "dev.burrdesign.de";
-		$config['LayoutClass'] = "burrdesign";
-		return $config;
 	}
 	
 	function prepareTextSQL($string){
@@ -60,12 +30,6 @@
 		$string = str_replace('€','&euro;',$string);
 		
 		return $string;
-	}
-
-	
-	function write_log($type,$message){
-		$query = "INSERT INTO bd_system_logs (logDate, logSession, logType, logMessage) VALUES (NOW(), '".$_SESSION['BURRDESIGN']['SESSIONID']."','".$type."','".$message."')";
-		$write = sqlquery($query,"insert");
 	}
 	
 ?>
