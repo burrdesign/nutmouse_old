4480e310cdd4518eced49b6baa0a61df04b544b5
julian <julian_burr@gmx.de>
2013-02-15 22:16:07 +0100

    Event- und Pluginsystem integriert

---
+++ core/classes/System/Event.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @Nutmouse CMS
+ * @Version: 0.2
+ * @Copyright: BurrDesign
+ * @Date: 2012-02-15
+ *
+ * @Beschreibung:
+ *		Event-Klasse zum triggern von Events und binden von bspw.
+ *		Plugins an diese
+ */
+
+class Event {
+
+	private static $bound = array();
+	private static $plugin_path = 'core/classes/Plugins';
+	
+	public static function bind($event, $class, $function){
+		//Pluginfunktion an Event binden (nur einmal!)
+		$boundclass = self::$bound[$event][$class];
+		for($i=0; $i<count($boundclass); $i++){
+			if($boundclass[$i] == $function){
+				return;
+			}
+		}
+		self::$bound[$event][$class][] = $function;
+	}
+	
+	public static function unbind($event, $class, $function){
+		//Gebundene Klassenfunktion wieder lösen
+		$boundclass = self::$bound[$event][$class];
+		for($i=0; $i<count($boundclass); $i++){
+			if($boundclass[$i] == $function){
+				unset($boundclass[$i]);
+			}
+		}
+	}
+	
+	public static function trigger($event){
+		//Event triggern und dadurch die gebundenen Funktionen auslösen
+		$to_trigger = self::$bound[$event];
+		foreach($to_trigger as $class => $functions){
+			include_once($_SERVER['DOCUMENT_ROOT'] . '/' . self::$plugin_path . '/' . $class . '.php');
+			$call = new $class();
+			foreach($functions as $function){
+				$call->$function();
+			}
+		}
+	}
+
+}
---
+++ core/classes/System/Plugin.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @Nutmouse CMS
+ * @Version: 0.2
+ * @Copyright: BurrDesign
+ * @Date: 2012-01-24
+ *
+ * @Beschreibung:
+ *		Vorlagen-Klasse für alle Pluginklassen
+ */
+
+class Plugin {
+
+	protected $info = array();
+
+	final public function __construct(){
+		//nichts machen!
+	}
+	
+	final protected function bind($event, $function){
+		//Pluginfunktion an Event binden
+		Event::bind($event, get_class($this), $function);
+	}
+	
+	public function install(){
+		//Plugin in die Datenbank schreiben
+		$this->info['pluginName'] = get_class($this);
+		$this->info['pluginInstalled'] = date("Y-m-d H:i:s", time());
+		
+		//Plugin in die SQL-Tabelle schreiben
+		$sql = new SqlManager();
+		$sql->insert('bd_sys_plugin',$this->info);
+	}
+	
+	public function uninstall(){
+		//Plugin aus der Datenbank löschen
+		$sql = new SqlManager();
+		$sql->setQuery("
+			SELECT * FROM bd_sys_plugin
+			WHERE pluginName = '{{Name}}'
+			LIMIT 1
+			");
+		$sql->bindParam("{{Name}}",get_class($this));
+		$uninstall = $sql->result();
+		if($uninstall['pluginKey']){
+			$sql->delete('bd_sys_plugin',$uninstall);
+		}
+	}
+	
+	public function init(){
+		//Initialisierung
+	}
+	
+}
