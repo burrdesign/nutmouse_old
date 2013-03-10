2dcab9cf7a2b6e189e7aa4cf0fe343dc6b6ceb5d
julian <julian_burr@gmx.de>
2013-03-10 01:02:32 +0100

    Event-Fehler behoben

--- core/classes/System/Event.php
+++ core/classes/System/Event.php
@@ -39,14 +39,16 @@ class Event {
 	
 	public static function trigger($event){
 		//Event triggern und dadurch die gebundenen Funktionen auslösen
+		$return = "";
 		$to_trigger = self::$bound[$event];
 		foreach($to_trigger as $class => $functions){
 			include_once($_SERVER['DOCUMENT_ROOT'] . '/' . self::$plugin_path . '/' . $class . '/' . $class . '.php');
 			$call = new $class();
 			foreach($functions as $function){
-				return $call->$function();
+				$return .= $call->$function();
 			}
 		}
+		return $return;
 	}
 
 }
