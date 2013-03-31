deff45fb6c024cbfdd67b005e37b86801319f7cc
julian <julian_burr@gmx.de>
2013-03-31 18:43:16 +0200

    kleine Änderung

--- core/config.php
+++ core/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$HOST = "db451751106.db.1and1.com";
-$USER = "dbo451751106";
-$PWD = "1234golf";
-$DB = "db451751106";
+$HOST = "localhost";
+$USER = "dbuser";
+$PWD = "dbpwd";
+$DB = "dbname";
